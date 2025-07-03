b612ef237404fe24757a11ca3fa218bab2222a36
pitchaya01 <32443867+pitchaya01@users.noreply.github.com>
2025-07-03 12:21:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,21 @@ resp = requests.get(
 
 current_price = resp.json()["bitcoin"]["usd"]
   
-
+result=''
 # ตรวจสอบว่าราคาอยู่ในช่วงหรือไม่
 if lower_bound <= current_price <= upper_bound:
-    print("Yes")
+    result="Yes"
 else:
-    print("No")
+     result="No"
+token = '7718053957:AAHSHEXigIC3lc9xkUgXtVlPWIg74eikYd0'
+chat_id = '6193006196'
+message = str(lower_bound)+'to'+str(upper_bound)+result
+
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+payload = {
+    'chat_id': chat_id,
+    'text': message
+}
+
+r = requests.post(url, data=payload)
+print(r.json())
