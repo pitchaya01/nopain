f3f9329ec5e036e152b9852744386108fc9c51b0
pitchaya01 <32443867+pitchaya01@users.noreply.github.com>
2025-08-17 11:30:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from datetime import datetime, timedelta
 import re
 import openai
 import os
-usernames = ["WorldMobilers"]
-#usernames = ["siamblockchain","BitcoinMagazine","IOHK_Charles","fairgatelabs","worldmobileteam", "MrTelecoms","CloverNodes","WMTxLady","hopenothype_io","wmchain","SebastienGllmt","MinswapIntern","ChristianRees","SebastienGllmt"]
+#usernames = ["WorldMobilers"]
+usernames = ["siamblockchain","BitcoinMagazine","IOHK_Charles","fairgatelabs","worldmobileteam", "MrTelecoms","CloverNodes","WMTxLady","hopenothype_io","wmchain","WorldMobilers","SebastienGllmt","MinswapIntern","ChristianRees","SebastienGllmt"]
 headers = {"X-API-Key": "e9636d13f522474b8bcfe3cad9c44d03"}
 token = '7718053957:AAHSHEXigIC3lc9xkUgXtVlPWIg74eikYd0'
 chat_id = '6193006196'
