7fb7d99df1ad608285947987d207781cca2e79d0
pitchaya01 <32443867+pitchaya01@users.noreply.github.com>
2025-11-04 16:02:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -118,15 +118,19 @@ def translate_to_thai(text):
 # r = requests.post(url, data=payload)
 # print(r)
 yesterday_utc = (datetime.utcnow() - timedelta(days=1)).date()
-data_a=get_price_coingecko(["minutes-network-token"],["usd"])
+data_a=get_price_coingecko(["bitcoin"],["usd"])
 data_b=get_price_coingecko(["world-mobile-token"],["usd"])
-price_a=float(data_a['minutes-network-token']['usd'])
+price_a=float(data_a['bitcoin']['usd'])
 price_b=float(data_b['world-mobile-token']['usd'])
-abs_p=round(normalized_abs_diff(price_a,price_b)*100,2)
-#send_telegram_text(abs_p,'7718053957:AAHSHEXigIC3lc9xkUgXtVlPWIg74eikYd0','6193006196')
+is_helthy=""
+if price_a > 100000:
+    is_helthy = "helth"
+
+#abs_p=round(normalized_abs_diff(price_a,price_b)*100,2)
+send_telegram_text(is_helthy,'7718053957:AAHSHEXigIC3lc9xkUgXtVlPWIg74eikYd0','6193006196')
 for uname in usernames:
     url = "https://api.twitterapi.io/twitter/user/last_tweets"
-    params = {"userName": uname,"includeReplies":"true"}
+    params = {"userName": uname,"includeReplies":"false"}
     response = requests.get(url, headers=headers, params=params)
     data = response.json()
  
